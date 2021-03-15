7d0f082a9ee1c0bbcd62ab2c5e95dfe985e0d8ff
brenosekiro <b88660501@gmail.com>
2021-03-15 14:55:57 -0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:todomobx/screens/login_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MobX Tutorial',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.deepPurpleAccent,
+        textSelectionTheme: TextSelectionThemeData(
+          cursorColor: Colors.deepPurpleAccent
+        ),
+        scaffoldBackgroundColor: Colors.deepPurpleAccent,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
