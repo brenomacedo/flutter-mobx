34746e639615ebd89108cc6ab044a36cea83feeb
brenosekiro <b88660501@gmail.com>
2021-03-15 16:33:46 -0300

    observableList

---
+++ lib/stores/list_store.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:mobx/mobx.dart';
+part 'list_store.g.dart';
+
+class ListStore = _ListStore with _$ListStore;
+
+abstract class _ListStore with Store {
+
+  @observable
+  String newTodoTitle = '';
+
+  @action
+  void setNewTodoTitle(String value) => newTodoTitle = value;
+
+  @computed
+  bool get empty => newTodoTitle.isEmpty;
+
+  @observable
+  ObservableList<String> todoList = ObservableList<String>();
+
+  @action
+  void addTodo() {
+    todoList.add(newTodoTitle);
+  }
+
+}
---
+++ lib/stores/list_store.g.dart
@@ -0,0 +1,81 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'list_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$ListStore on _ListStore, Store {
+  Computed<bool> _$emptyComputed;
+
+  @override
+  bool get empty => (_$emptyComputed ??=
+          Computed<bool>(() => super.empty, name: '_ListStore.empty'))
+      .value;
+
+  final _$newTodoTitleAtom = Atom(name: '_ListStore.newTodoTitle');
+
+  @override
+  String get newTodoTitle {
+    _$newTodoTitleAtom.reportRead();
+    return super.newTodoTitle;
+  }
+
+  @override
+  set newTodoTitle(String value) {
+    _$newTodoTitleAtom.reportWrite(value, super.newTodoTitle, () {
+      super.newTodoTitle = value;
+    });
+  }
+
+  final _$todoListAtom = Atom(name: '_ListStore.todoList');
+
+  @override
+  ObservableList<String> get todoList {
+    _$todoListAtom.reportRead();
+    return super.todoList;
+  }
+
+  @override
+  set todoList(ObservableList<String> value) {
+    _$todoListAtom.reportWrite(value, super.todoList, () {
+      super.todoList = value;
+    });
+  }
+
+  final _$_ListStoreActionController = ActionController(name: '_ListStore');
+
+  @override
+  void setNewTodoTitle(String value) {
+    final _$actionInfo = _$_ListStoreActionController.startAction(
+        name: '_ListStore.setNewTodoTitle');
+    try {
+      return super.setNewTodoTitle(value);
+    } finally {
+      _$_ListStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void addTodo() {
+    final _$actionInfo =
+        _$_ListStoreActionController.startAction(name: '_ListStore.addTodo');
+    try {
+      return super.addTodo();
+    } finally {
+      _$_ListStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+newTodoTitle: ${newTodoTitle},
+todoList: ${todoList},
+empty: ${empty}
+    ''';
+  }
+}
